4a6380e05e63519d081e92898db4ab0af3303337
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-11-28 20:34:41 +0100

    Update restore_raspberry.sh

--- restore_raspberry.sh
+++ restore_raspberry.sh
@@ -3,4 +3,4 @@
 DEST_DISK=$1
 BACKUP_FILE=$2
 
-gunzip -c $BACKUP_FILE.image.gz | dd conv=noerror,sync status=progress of=dev/$DEST_DISK
+gunzip -c $BACKUP_FILE.image.gz | dd conv=noerror,sync status=progress of=/dev/$DEST_DISK
